516286fe3b770163c615f13330f44485f6e66456
余磊 <yulei@addcn.com>
2022-01-27 17:40:08 +0800

    chore: use swc instead of esbuild

--- test/import-mjs/index.ts
+++ test/import-mjs/index.ts
@@ -1,6 +1,6 @@
 import foo from 'foo'
 import notIndex from 'foo/not-index'
-import notIndex from 'foo/not-index.js'
+// import notIndex from 'foo/not-index.js' # 等待官方修復中 https://github.com/swc-project/swc/pull/3194
 import { a } from '../fixture.export'
 import { hello } from 'with-dts/hello'
 
---
+++ test/swc.entry.ts
@@ -0,0 +1,137 @@
+import assert from 'assert'
+import { relative } from 'path'
+import { test } from 'uvu'
+import execa from 'execa'
+
+const cwd = process.cwd()
+function relativize(path: string, curCwd = cwd) {
+  return `./${relative(curCwd, path)}`
+}
+
+test('register', async() => {
+  const { stdout } = await execa('node', [
+    '--experimental-loader',
+    relativize(`${cwd}/swc-loader.mjs`),
+    relativize(`${cwd}/test/fixture.ts`),
+  ])
+  assert(stdout === 'text')
+})
+
+test('register2', async() => {
+  const { stdout } = await execa('node', [
+    '--experimental-loader',
+    relativize(`${cwd}/swc-loader.mjs`),
+    relativize(`${cwd}/test/fixture.arrowFunction.ts`),
+  ])
+  assert(stdout === 'hello from ts')
+})
+
+test('register3', async() => {
+  const { stdout } = await execa('node', [
+    '--experimental-loader',
+    relativize(`${cwd}/swc-loader.mjs`),
+    relativize(`${cwd}/test/fixture.import.ts`),
+  ])
+  assert(stdout === 'export')
+})
+
+test('register4', async() => {
+  const { stdout } = await execa('node', [
+    '--experimental-loader',
+    relativize(`${cwd}/swc-loader.mjs`),
+    relativize(`${cwd}/test/fixture.optionalChaining.ts`),
+  ])
+  assert(stdout === 'hello from ts')
+})
+
+test('register cjs', async() => {
+  const { stdout } = await execa('node', [
+    '--experimental-loader',
+    relativize(`${cwd}/swc-loader.mjs`),
+    relativize(`${cwd}/test/fixture.cjs.ts`),
+  ])
+  assert(stdout === 'fs imported')
+})
+
+test('register mts', async() => {
+  const { stdout } = await execa('node', [
+    '--experimental-loader',
+    relativize(`${cwd}/swc-loader.mjs`),
+    relativize(`${cwd}/test/fixture.mts.mts`),
+  ])
+  assert(stdout === 'hello from mts')
+})
+
+test('package type module', async() => {
+  const { stdout } = await execa('node', [
+    '--experimental-loader',
+    relativize(`${cwd}/swc-loader.mjs`),
+    relativize(`${cwd}/test/fixture-type-module/index.js`),
+  ])
+  assert(stdout === 'foo')
+})
+
+test('import type module', async() => {
+  const { stdout } = await execa('node', [
+    '--experimental-loader',
+    relativize(`${cwd}/swc-loader.mjs`),
+    relativize(`${cwd}/test/import-mjs/index.ts`),
+  ])
+  assert(stdout === 'foo\nnot index\nexport\nhello from d.ts')
+})
+
+test('import with query', async() => {
+  const { stdout } = await execa('node', [
+    '--experimental-loader',
+    relativize(`${cwd}/swc-loader.mjs`),
+    relativize(`${cwd}/test/fixture.importWithQuery.ts`),
+  ])
+  assert(stdout === '1\n2')
+})
+
+test('import tsx', async() => {
+  const { stdout } = await execa('node', [
+    '--experimental-loader',
+    relativize(`${cwd}/swc-loader.mjs`),
+    relativize(`${cwd}/test/fixture.tsxStyle.tsx`),
+  ])
+  assert(stdout === 'foo:bar')
+})
+
+test('import tsx2', async() => {
+  const { stdout } = await execa('node', [
+    '--experimental-loader',
+    relativize(`${cwd}/swc-loader.mjs`),
+    relativize(`${cwd}/test/fixture.query2.ts`),
+  ])
+  assert(stdout === 'foo:bar')
+})
+
+
+/**
+ * swc 目前還不支持 .json 文件的載入
+ */
+// test('import json', async() => {
+//   const { stdout } = await execa('node', [
+    
+//     '--experimental-loader',
+//     relativize(`${cwd}/swc-loader.mjs`),
+//     relativize(`${cwd}/test/fixture.json.ts`),
+//   ])
+
+//   assert(stdout === 'esbuild-node-loader')
+// })
+
+test('tsconfig-paths', async() => {
+  const cwd2 = `${cwd}/test/tsconfig-paths`
+  const { stdout } = await execa('node', [
+    '--experimental-loader',
+    relativize(`${cwd}/swc-loader.mjs`, cwd2),
+    relativize(`${cwd}/test/tsconfig-paths/src/utils/fixture.ts`, cwd2),
+  ], {
+    cwd: cwd2,
+  })
+  assert.equal(stdout, 'foo\nfoo')
+})
+
+test.run()
